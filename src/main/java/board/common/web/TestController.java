17edf12f5a88438658ba6e982e32b0e69c0f2cec
Insuk Choi <insuk@Insukui-MacBook-Pro.local>
2017-04-20 19:37:08 +0900

    first commit

---
+++ src/main/java/board/common/web/TestController.java
@@ -0,0 +1,35 @@
+package board.common.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by insuk on 2017. 4. 15..
+ */
+@Controller
+public class TestController {
+
+    protected Logger logger = Logger.getLogger(this.getClass());
+
+    @RequestMapping("/")
+    public String Index(Model model) throws Exception
+    {
+        logger.info("index ....");
+        return "home";
+    }
+
+    @RequestMapping("/main.do")
+    public String Main(Model model) throws Exception
+    {
+        logger.info("main ....");
+
+        // DB
+
+        model.addAttribute("spring","spring");
+        return "home1";
+    }
+
+
+}
---
+++ target/board/jsp/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: insuk
+  Date: 2017. 4. 15.
+  Time: PM 2:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    ${spring} ! Hello, World
+</body>
+</html>
