ef1e83690f44932caaf283375881572866447ce8
Hyeshin <amist04@naver.com>
2017-05-03 16:21:42 +0900

    dkssyd

--- src/main/java/board/common/error/errorController.java
+++ src/main/java/board/common/error/errorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by admin on 2017-04-28.
+ * Created by admin on 2017-05-03.
  */
 public class errorController {
 
@@ -15,7 +15,7 @@ public class errorController {
     @RequestMapping(value = "/error404", method = RequestMethod.GET)
     public String error404(Model model) throws Exception
     {
-        logger.warn("404 error occurred");
+        logger.warn("404 error occurred...");
 
         return "view/404_error_view";
     }
